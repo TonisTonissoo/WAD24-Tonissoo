7b18bf1349c731652c7a6303ee2fcc34e0e83871
tonistonissoo-admin <toniston@ut.ee>
2025-01-17 12:29:14 +0200

    Task1

--- src/views/Students.vue
+++ src/views/Students.vue
@@ -1,13 +1,31 @@
 <template>
-<div>
-<h3> Students grades </h3>
+  <div>
+<h3> Students grades  </h3>
 
-
-<div class="container">
-  <!-- Table go here -->
-
+<!-- Task 1 -->
+    <div class="container">
+          <table>
+          <thead>
+          <tr>
+            <th>St.Code</th>
+            <th>St.Level</th>
+            <th>Homeworks</th>
+            <th>Exam</th>
+            <th>Final grade</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="item" v-for="grade in grades" :key="grade.id">
+            <td>{{ grade.studentcode}}</td>
+            <td>{{ grade.studentlevel}}</td>
+            <td>{{ grade.hws}}</td>
+            <td>{{ grade.exam}}</td>
+            <td>{{ grade.final}}</td>
+          </tr>
+          </tbody>
+          </table>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
