b4a70cb32b422c9f717b92adf2b06c2c7da0777b
tonistonissoo-admin <toniston@ut.ee>
2025-01-17 14:16:01 +0200

    Task9

--- cypress/e2e/exam2-tests.cy.js
+++ cypress/e2e/exam2-tests.cy.js
@@ -1,5 +1,10 @@
 /// <reference types = "cypress"/>  
-
+it('Test 1', () => {
+    cy.visit('http://localhost:8080/api/Instructor')
+    cy.wait(2000)
+    cy.contains("Instructor Management Pannel")
+    cy.get('td.Homeworks').first().click
+}) 
 
 
 
